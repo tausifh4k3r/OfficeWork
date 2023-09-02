e15334ad44699fd8fc398f575a80cb0fd87e065e
TAUSIF BASHIR PATEL <61449709+tausifh4k3r@users.noreply.github.com>
2023-09-03 03:53:24 +0530

    Delete Check Duplicate Name directory

--- Check Duplicate Name/File_get_content_api.php
+++
@@ -1,7 +0,0 @@
-<?php
-// proxy.php
-$URL = $_POST['Url'];
-
-// $externalLink = "https://info.microsoft.com/ww-landing-accelerate-sales-and-revenue-with-dynamics-365.html?LCID=ES";
-echo file_get_contents($URL);
-?>
